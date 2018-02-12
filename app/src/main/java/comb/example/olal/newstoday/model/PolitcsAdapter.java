7a193587ea2de42c76f9477e921d7efc774ad2cd
Olal Keith <jkeneth.jk@gmail.com>
2018-02-12 15:46:57 +0300

    urlToImage on a cardView

--- app/src/main/java/comb/example/olal/newstoday/model/PolitcsAdapter.java
+++ app/src/main/java/comb/example/olal/newstoday/model/PolitcsAdapter.java
@@ -5,8 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,48 +21,47 @@ import comb.example.olal.newstoday.SourcesAdapter;
 
 public class PolitcsAdapter extends RecyclerView.Adapter<PolitcsAdapter.ViewHolder> {
 
-    public Activity activity;
-    private List<Article> articles;
+  public Activity activity;
+  private List<Article> articles;
 
-    public PolitcsAdapter(Activity activity, List<Article> articles){
+  public PolitcsAdapter(Activity activity, List<Article> articles) {
 
-        this.articles = articles;
-        this.activity = activity;
+    this.articles = articles;
+    this.activity = activity;
+  }
+
+  @Override public PolitcsAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+    View view = LayoutInflater.from(viewGroup.getContext())
+        .inflate(R.layout.politics_card, viewGroup, false);
+    return new ViewHolder(view);
+  }
+
+  @Override public void onBindViewHolder(PolitcsAdapter.ViewHolder viewHolder, int position) {
+    Article article = articles.get(position);
+    viewHolder.sourcesTitle.setText(article.getTitle());
+    viewHolder.sourcesName.setText(article.getPoliticssource().getName());
+    viewHolder.sourcesDescription.setText(article.getDescription());
+    viewHolder.sourcesPublishedAt.setText(article.getPublishedAt());
+
+    Picasso.with(activity).load(article.getUrlToImage()).into(viewHolder.sourcesImage);
+  }
+
+  @Override public int getItemCount() {
+    return articles.size();
+  }
+
+  public class ViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView sourcesName, sourcesDescription, sourcesPublishedAt, sourcesTitle;
+    private ImageView sourcesImage;
+
+    public ViewHolder(View itemView) {
+      super(itemView);
+      sourcesImage = (ImageView) itemView.findViewById(R.id.image_url);
+      sourcesTitle = (TextView) itemView.findViewById(R.id.sources_title);
+      sourcesName = (TextView) itemView.findViewById(R.id.sources_name);
+      sourcesDescription = (TextView) itemView.findViewById(R.id.sources_description);
+      sourcesPublishedAt = (TextView) itemView.findViewById(R.id.sources_publishedAt);
     }
-
-
-    @Override
-    public PolitcsAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.politics_card,viewGroup,false);
-        return new ViewHolder(view);
-
-    }
-
-    @Override
-    public void onBindViewHolder(PolitcsAdapter.ViewHolder viewHolder, int position) {
-Article article = articles.get(position);
-        viewHolder.sourcesName.setText(article.getPoliticssource().getName());
-        viewHolder.sourcesDescription.setText(article.getDescription());
-        viewHolder.sourcesPublishedAt.setText(article.getPublishedAt());
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return articles.size();
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder{
-
-        private TextView sourcesName,sourcesDescription,sourcesPublishedAt;
-
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-
-            sourcesName = (TextView)itemView.findViewById(R.id.sources_name);
-            sourcesDescription = (TextView)itemView.findViewById(R.id.sources_description);
-            sourcesPublishedAt = (TextView)itemView.findViewById(R.id.sources_publishedAt);
-        }
-    }
+  }
 }
