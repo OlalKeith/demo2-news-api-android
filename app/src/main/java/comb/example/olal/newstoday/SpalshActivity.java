1daca599c37181ff1dc1490d2075657fd63130c5
Olal Keith <jkeneth.jk@gmail.com>
2018-02-15 17:17:05 +0300

    splash screen with transition animation

---
+++ app/src/main/java/comb/example/olal/newstoday/SpalshActivity.java
@@ -0,0 +1,42 @@
+package comb.example.olal.newstoday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SpalshActivity extends AppCompatActivity {
+
+  private TextView tv;
+  private ImageView iv;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_spalsh);
+    tv = (TextView) findViewById(R.id.tv);
+    iv = (ImageView) findViewById(R.id.iv);
+    Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+    tv.startAnimation( myanim);
+    iv.startAnimation(myanim);
+    final Intent intent = new Intent(this,MainActivity.class);
+    Thread timer = new Thread(){
+      public void run (){
+
+        try {
+          sleep(5000);
+        }catch (InterruptedException e){
+          e.printStackTrace();
+        }
+
+        finally {
+          startActivity(intent);
+          finish();
+        }
+      }
+    };
+    timer.start();
+  }
+}
